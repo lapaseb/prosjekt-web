f8351f3276b1bba156c92eb29ede023f0c2be796
lapaseb <sebastien.lapaire@divtec.ch>
2016-11-25 16:12:58 +0100

    Modification du login form

--- app/login/login.js
+++ app/login/login.js
@@ -12,5 +12,19 @@ angular.module('myApp.login',  ['ngRoute'])
 .controller('LoginCtrl', ['$rootScope', '$scope', '$location', function($rootScope, $scope, $location) {
   $rootScope.hideit = true;
 
+  $scope.SignIn = function(e){
+     e.preventDefault();
+     // On stock les valeurs fournies dans le formulaire de connexion
+     var email    = $scope.user.email;
+     var password = $scope.user.password;
+     firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
+          $location.path("#!/view1");
+      }, function(error){
+          // On affiche les erreurs si la connexion a échouée
+          $scope.regError = true;
+          $scope.regErrorMessage = error.message;
+          alert($scope.regErrorMessage);
+      });
+  }
 
 }]);
--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,6 +9,6 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', [function() {
-
+.controller('View1Ctrl', [ '$rootScope', '$scope', '$location', function($rootScope) {
+  $rootScope.hideit = false;
 }]);
