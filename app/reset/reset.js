1ec23f9ea0d2c5f7d97d0a9537b5ae20f175f9b4
Sébastien <sebastien.brainlolz@gmail.com>
2016-12-10 15:28:53 +0100

    Page dédiée pour la réintialisation du mot de passe

--- app/reset/reset.js
+++ app/reset/reset.js
@@ -3,7 +3,7 @@
 angular.module('myApp.reset',  ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/login', {
+  $routeProvider.when('/reset', {
     templateUrl: 'reset/reset.html',
     controller: 'ResetCtrl'
   });
@@ -20,56 +20,22 @@ angular.module('myApp.reset',  ['ngRoute'])
     }
   });
 
-
-  $scope.SignIn = function(e){
-     e.preventDefault();
-     // On stock les valeurs fournies dans le formulaire de connexion
-     var email    = $scope.user.email;
-     var password = $scope.user.password;
-
-     firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
-       $rootScope.$apply(function() {
-         $location.path("/projects");
-       });
-      }, function(error){
-          // On affiche les erreurs si la connexion a échouée
-          $scope.regError = true;
-          $scope.regErrorMessage = error.message;
-          alert($scope.regErrorMessage);
-      });
-  }
-
-  $scope.signInGoogle = function(){
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-      var token = result.credential.accessToken;
-      var user = result.user;
-    }).catch(function(error) {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      var email = error.email;
-      var credential = error.credential;
-      alert(error);
-    });
-  }
-
   $scope.resetPassword = function(){
     var email    = $scope.user.email;
-    var auth = firebase.auth();
+    var auth     = firebase.auth();
 
     if (email == null){
       Materialize.toast("indiquez votre adresse email dans le champ prévu à cet effet.", 4000);
     } else {
       auth.sendPasswordResetEmail(email).then(function() {
         Materialize.toast("Un email de réinitialiser vous a été envoyé à l\'adresse " + email, 4000);
+        $rootScope.$apply(function() {
+          $location.path("/login");
+        });
       }, function(error) {
         Materialize.toast("Une erreur s'est produite.", 4000);
       });
     }
-
-
-
-
   }
 
 }]);
