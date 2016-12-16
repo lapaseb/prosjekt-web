e06087300d5e8079a6f0cc4a03d092b4ea6277aa
lapaseb <sebastien.lapaire@divtec.ch>
2016-12-16 16:02:16 +0100

    Gestion progression tâche principale en fonction des tâches secondaires

--- app/todo/todo.js
+++ app/todo/todo.js
@@ -29,21 +29,54 @@ angular.module('myApp.todo', ['ngRoute'])
   firebase.auth().onAuthStateChanged(function(user) {
 
 
+
     $scope.setTodoState = function(todoID, subTodoID){
 
-      var ref = firebase.database().ref().child(user.uid + "/projets/" + $routeParams.projectID + "/tachesPrincipales/" +  todoID + "/tachesSecondaire/" + subTodoID + "/isOver/");
-      var isOver = $firebaseObject(ref);
+      var SubTodos = firebase.database().ref().child(user.uid + "/projets/" + $routeParams.projectID + "/tachesPrincipales/" +  todoID + "/tachesSecondaire/");
+      var SubTodosArray = $firebaseArray(SubTodos);
+      var nbreTodoOver = 0;
 
+      SubTodosArray.$loaded().then(function(){
 
-      var onValueChange = ref.once('value', function(dataSnapshot) {
-
-        if (dataSnapshot.val() == false){
-          ref.set(true);
-        }else {
-          ref.set(false);
+        for (var i = 0; i < SubTodosArray.length ; i++){
+          if (SubTodosArray.$getRecord(SubTodosArray.$keyAt(i)).isOver == true){
+            nbreTodoOver++;
+          }
         }
 
-      });
+        var refProgression = firebase.database().ref().child(user.uid + "/projets/" + $routeParams.projectID + "/tachesPrincipales/" +  todoID + "/progression/");
+        var refProgressionToShow = $firebaseObject(refProgression);
+
+
+
+        var onValueChange = refProgression.once('value', function(dataSnapshot) {
+        });
+
+        var ref = firebase.database().ref().child(user.uid + "/projets/" + $routeParams.projectID + "/tachesPrincipales/" +  todoID + "/tachesSecondaire/" + subTodoID + "/isOver/");
+        var isOver = $firebaseObject(ref);
+
+        var onValueChange = ref.once('value', function(dataSnapshot) {
+
+          refProgressionToShow.$loaded().then(function(){
+
+
+            if (dataSnapshot.val() == false){
+              nbreTodoOver++;
+              ref.set(true);
+              refProgression.set(refProgressionToShow.$value + (100/SubTodosArray.length));
+            }else {
+              nbreTodoOver--;
+              ref.set(false);
+              refProgression.set(refProgressionToShow.$value - (100/SubTodosArray.length));
+            }
+
+          })
+        });
+
+      })
+
+
+
 
     }
 
